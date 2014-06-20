e9823a749041a863aeae090b6f2fa2f37556bc98
Craig Dennis <craigmdennis@gmail.com>
2014-06-20 01:00:11 -0700

    chore: update deploy configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,32 +7,9 @@ set :repo_url, 'git@bitbucket.org:simplebitdesign/localwhistler.com.git'
 # Default branch is :master
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
-
-# Default value for linked_dirs is []
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :scm, :git
+set :deploy_via, :remote_cache
+set :copy_exclude, [".git", ".DS_Store", ".gitignore", ".gitmodules"]
 
 namespace :deploy do
 
@@ -44,12 +21,12 @@ namespace :deploy do
     end
   end
 
-  desc "Backup MySQL Database"
-  task :mysqlbackup, :roles => :app do
-    run "mysqldump -u#{db_username} -p#{db_password} #{db_database} > #{shared_path}/backups/#{release_name}.sql"
-  end
-
-  after 'deploy:publishing', 'gridserver:create_relative_symlinks'
+  # desc "Backup MySQL Database"
+  # task :mysqlbackup do
+  #   on roles(:app), in: :groups do
+  #     run "mysqldump -u#{db_username} -p#{db_password} #{db_database} > #{shared_path}/backups/#{release_name}.sql"
+  #   end
+  # end
 
   after :publishing, :restart
 
@@ -63,3 +40,16 @@ namespace :deploy do
   end
 
 end
+
+# Local Whistler specific tasks namespaced to reduce conflict
+namespace :localwhister do
+
+  desc 'Point uploads directory to shared folder'
+  task :symlink
+    on roles(:app), in: :groups do
+      execute "ln -nfs #{shared_path}/uploads #{release_path}/content/uploads"
+    end
+
+end
+
+after "deploy:symlink:release", "localwhister:symlink"
