a3ecba124d592ddddb6cb787c774101ef654cb2c
Craig Dennis <craigmdennis@gmail.com>
2014-08-17 20:56:00 -0700

    chore: change staging branch

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :application, 'lw-dev.simplebitdesign.com'
 set :deploy_to, '/home/152547/users/.home/domains/lw-dev.simplebitdesign.com/'
-set :branch, 'staging'
+set :branch, 'develop'
 
 server 'simplebitdesign.com', user: 'simplebitdesign.com', roles: %w{web app}
 
