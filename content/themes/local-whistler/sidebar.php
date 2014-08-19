0f49db57780129d16e034a7c955bebd49804a6b8
Craig Dennis <craigmdennis@gmail.com>
2014-08-18 21:25:55 -0700

    chore: remove sidebar content

--- content/themes/local-whistler/sidebar.php
+++ content/themes/local-whistler/sidebar.php
@@ -1,6 +1,2 @@
-<?php ?>
-<section role="complementary" class="secondary-content">
-	<?php if ( !function_exists('dynamic_sidebar')
-        || !dynamic_sidebar() ) : ?>
-	<?php endif; ?>
-</section>
+<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar() ) : ?>
+<?php endif; ?>
