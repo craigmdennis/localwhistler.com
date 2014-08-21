c359a7653993f849fde5578678f5dfaf880de560
Craig Dennis <craigmdennis@gmail.com>
2014-08-21 15:50:35 -0700

    chore: add icons

--- content/themes/local-whistler/partials/_toolbar.php
+++ content/themes/local-whistler/partials/_toolbar.php
@@ -8,24 +8,20 @@
     <?php endif; ?>
   </div>
 
-  <ul id="views" class="toolbar__actions">
-    <li class="toolbar__item">
-      <a href="<?php echo add_query_arg( 'view', 'gallery' ); ?>" class="toolbar__button">
-        <span class="toolbar__button-text">Gallery</span>
-        <i class="toolbar__ion icon-picture"></i>
+    <div class="btn-group toolbar__actions">
+
+      <a href="<?php echo add_query_arg( 'view', 'gallery' ); ?>" class="btn btn--default btn--icon-only" title="View as a gallery">
+        <span class="btn__text">Gallery</span>
+        <i class="btn__icon icon--after icon-thumbnails"></i>
       </a>
-      </li>
-    <li class="toolbar__item">
-      <a href="<?php echo add_query_arg( 'view', 'list' ); ?>" class="toolbar__button">
-        <span class="toolbar__button-text">List</span>
-        <i class="toolbar__ion icon-list"></i>
+
+      <a href="<?php echo add_query_arg( 'view', 'list' ); ?>" class="btn btn--default btn--icon-only" title="View as a list">
+        <span class="btn__text">List</span>
+        <i class="btn__icon icon--after icon-list"></i>
       </a>
-    </li>
-    <li class="toolbar__item show-with-js">
-      <a href="<?php echo add_query_arg( 'view', 'map' ); ?>" class="toolbar__button">
-        <span class="toolbar__button-text">Map</span>
-        <i class="toolbar__ion icon-list"></i>
+
+      <a href="<?php echo add_query_arg( 'view', 'map' ); ?>" class="btn btn--default btn--icon-only show-with-js" title="View on a map">
+        <span class="btn__text">Map</span>
+        <i class="btn__icon icon--after icon-map"></i>
       </a>
-    </li>
-  </ul>
 </div>
