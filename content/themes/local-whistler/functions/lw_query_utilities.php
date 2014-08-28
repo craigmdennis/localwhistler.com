7ca57361164d566764c2d20c9815e6b787ccfb2c
Craig Dennis <craigmdennis@gmail.com>
2014-08-28 02:10:41 -0700

    refactor: split out JSON functions

--- content/themes/local-whistler/functions/lw_query_utilities.php
+++ content/themes/local-whistler/functions/lw_query_utilities.php
@@ -7,7 +7,6 @@
   // --------------------------------------------------------------------------//
 
   add_filter( 'query_vars', 'add_query_vars_filter' );
-  add_filter('json_api_encode', 'json_api_encode_acf');
 
 
 
@@ -110,29 +109,6 @@
 
     return $viewType;
 
-  }
-
-
-
-  // Add ACF fields to JSON API ---------------------------------------------- //
-
-  function json_api_encode_acf($response)
-  {
-      if (isset($response['posts'])) {
-          foreach ($response['posts'] as $post) {
-              json_api_add_acf($post); // Add specs to each post
-          }
-      }
-      else if (isset($response['post'])) {
-          json_api_add_acf($response['post']); // Add a specs property
-      }
-
-      return $response;
-  }
-
-  function json_api_add_acf(&$post)
-  {
-      $post->acf = get_fields($post->id);
   }
 
 ?>
