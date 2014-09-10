448732ef9ba6170b1bfbb7fc629784169278d563
Craig Dennis <craigmdennis@gmail.com>
2014-09-10 15:10:46 -0700

    fix: issue #72
    Added is_post_type_archive() check

--- content/themes/local-whistler/index.php
+++ content/themes/local-whistler/index.php
@@ -4,7 +4,8 @@
 
   <?php
 
-    if ( isset( $_GET['business_type'] ) && isset( $_GET['business_location'] ) ) :
+    // Redirect all references to businesses to use the taxonomy (filtering) template
+    if ( ( isset( $_GET[ 'business_type' ] ) && isset( $_GET[ 'business_location' ] ) ) || is_post_type_archive( 'business' ) ) :
       get_template_part( 'taxonomy' );
     else:
       get_template_part( 'partials/loop', 'index' );
