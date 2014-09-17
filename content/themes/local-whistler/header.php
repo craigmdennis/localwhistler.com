23ca2ba271b8eb828191d765f43ad848f272dce2
Craig Dennis <craigmdennis@gmail.com>
2014-09-17 00:14:14 -0700

    fix: simplify title and description

--- content/themes/local-whistler/header.php
+++ content/themes/local-whistler/header.php
@@ -27,25 +27,8 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <title>
-    <?php
-      global $page, $paged;
-
-      wp_title( '|', true, 'right' );
-      bloginfo( 'name' );
-      $site_description = get_bloginfo( 'description', 'display' );
-      if ( $site_description && ( is_home() || is_front_page() ) )
-        echo " | $site_description";
-      if ( $paged >= 2 || $page >= 2 )
-        echo ' | ' . sprintf( __( 'Page %s' ), max( $paged, $page ) );
-    ?>
-  </title>
-  <meta name="description" content="<?php if ( is_single() ) {
-    single_post_title('', true);
-    } else {
-    bloginfo('name'); echo " - "; bloginfo('description');
-    }
-  ?>" />
+  <title><?php wp_title(''); ?></title>
+  <meta name="description" content="<?php bloginfo('description'); ?>" />
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
   <meta name="viewport" content="width=device-width" />
 
