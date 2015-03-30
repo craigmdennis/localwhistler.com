f0c82d76a89889e30c57f288b4a17bdf8d007388
Craig Dennis <craigmdennis@gmail.com>
2015-03-30 15:37:49 -0700

    chore: Add sample local-config file

---
+++ local-config-sample.php
@@ -0,0 +1,18 @@
+<?php
+
+  /** The name of the database for WordPress */
+  define('DB_NAME', 'localwhister_wp');
+
+  /** MySQL database username */
+  define('DB_USER', 'localwhister');
+
+  /** MySQL database password */
+  define('DB_PASSWORD', '');
+
+  /** MySQL hostname */
+  define('DB_HOST', 'localhost');
+
+  /** Server name (your url) */
+  $domain_name = 'http://localwhistler.local';
+
+?>
